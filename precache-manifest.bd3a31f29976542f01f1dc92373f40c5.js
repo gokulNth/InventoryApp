c1d760f4c737e7da2edcbd91e7036cd623f7c33c
GokulnthK <gokul1997in12@gmail.com>
2019-08-22 16:06:00 +0530

    Updates

---
+++ precache-manifest.bd3a31f29976542f01f1dc92373f40c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d5bfc5a6a27c6179ff82ab7736a3e7",
+    "url": "/InventoryApp/index.html"
+  },
+  {
+    "revision": "aeaadb41b29e24187f0a",
+    "url": "/InventoryApp/static/js/2.6c796f95.chunk.js"
+  },
+  {
+    "revision": "6bb5419b61a0fc9a8510",
+    "url": "/InventoryApp/static/js/main.1493ee31.chunk.js"
+  },
+  {
+    "revision": "1b32bc87b13e55e24b6b",
+    "url": "/InventoryApp/static/js/runtime~main.2f401b8a.js"
+  },
+  {
+    "revision": "817596d6626736251eea50f61b9492a4",
+    "url": "/InventoryApp/static/media/deleting.817596d6.gif"
+  },
+  {
+    "revision": "ed23685339ada1b6d88008cbe1a11e98",
+    "url": "/InventoryApp/static/media/loading.ed236853.gif"
+  }
+]);
